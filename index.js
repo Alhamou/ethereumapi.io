a6fb7055e8e4d31f726509f11b9878580119137b
Emad Alhamou <emadalhamo@gmail.com>
2022-01-01 16:45:16 +0100

    add postman collection

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ require('dotenv').config()
 const express = require("express")
 const app = new express()
 
-var options = {
-    dotfiles: 'ignore',
-    etag: false,
-    extensions: ['htm', 'html'],
-    index: false,
-    maxAge: '1d',
-    redirect: false,
-    setHeaders: function (res, path, stat) {
-      res.set('x-timestamp', Date.now())
-    }
-}
+// const options = {
+//     dotfiles: 'ignore',
+//     etag: false,
+//     extensions: ['htm', 'html'],
+//     index: false,
+//     maxAge: '1d',
+//     redirect: false,
+//     setHeaders: function (res, path, stat) {
+//       res.set('x-timestamp', Date.now())
+//     }
+// }
 
 
 app.use(express.static('public', options));
